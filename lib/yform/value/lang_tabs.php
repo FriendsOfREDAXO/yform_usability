704e1faa20ac6ca90d916f931776c8ed9b1ec90c
Alex Platter <alex.platter@gmail.com>
2021-03-31 15:55:29 +0200

    new yform value lang_tabs added

---
+++ lib/yform/value/lang_tabs.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * yform
+ * @author Kreatif GmbH
+ * @author <a href="http://www.kreatif.it">www.kreatif.it</a>
+ */
+class rex_yform_value_lang_tabs extends rex_yform_value_abstract
+{
+
+    function enterObject()
+    {
+        $this->params['form_output'][$this->getId()] = $this->parse('value.lang_tabs.tpl.php');
+    }
+
+    function getDescription()
+    {
+        return htmlspecialchars('tab_start');
+    }
+
+    function getDefinitions($values = [])
+    {
+        return [
+            'type'            => 'value',
+            'name'            => 'lang_tabs',
+            'description'     => rex_i18n::msg('yform_usability.lang_tabs_description'),
+            'values'          => [
+                'name'    => ['type' => 'name', 'label' => rex_i18n::msg('yform_values_defaults_name')],
+                'partial' => [
+                    'type'    => 'choice',
+                    'label'   => rex_i18n::msg('yform_usability.partial'),
+                    'choices' => json_encode([
+                        rex_i18n::msg('yform_usability.tab_start') => 'start',
+                        rex_i18n::msg('yform_usability.tab_break') => 'break',
+                        rex_i18n::msg('yform_usability.tab_end')   => 'end',
+                    ]),
+                ],
+            ],
+            'dbtype'          => 'none',
+            'is_hiddeninlist' => true,
+            'is_searchable'   => false,
+            'famous'          => true,
+        ];
+    }
+
+}
---
+++ ytemplates/bootstrap/value.lang_tabs.tpl.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @author Kreatif GmbH
+ * @author a.platter@kreatif.it
+ * Date: 31.03.21
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$tabIds = rex::getProperty('yform_usability.lang_tab_ids', []);
+
+?>
+<?php if ($this->getElement('partial') == 'start'): ?>
+<div class="nav rex-page-nav yform-lang-tabs">
+    <ul class="nav nav-tabs">
+        <?php
+        $langs   = rex_clang::getAll();
+        $clangId = rex_clang::getCurrentId();
+        ?>
+        <?php foreach ($langs as $lang): ?>
+            <?php
+            $tabId    = $this->getFieldId() .'-'. $lang->getId();
+            $tabIds[] = $tabId;
+            ?>
+            <li class="<?= $lang->getId() == $clangId ? 'active' : '' ?>">
+                <a href="#form-tab-content-col-<?= $tabId ?>" data-toggle="tab">
+                    <?= $lang->getName() ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+    <div class="tab-content">
+        <div class="tab-pane fade in active" id="form-tab-content-col-<?= array_shift($tabIds) ?>">
+        <?php endif; ?>
+
+
+        <?php if ($this->getElement('partial') == 'break'): ?>
+        </div>
+        <div class="tab-pane fade" id="form-tab-content-col-<?= array_shift($tabIds) ?>">
+        <?php endif; ?>
+
+
+        <?php if ($this->getElement('partial') == 'end'): ?>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
+
+<?php
+
+rex::setProperty('yform_usability.lang_tab_ids', $tabIds);
