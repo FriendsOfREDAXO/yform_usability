d7ca9cb68b29e99031dfeff5380a8024cf764415
Alex Platter <alex.platter@gmail.com>
2021-09-20 22:23:21 +0200

    rex_i18n::translate added

--- lib/Utils.php
+++ lib/Utils.php
@@ -73,7 +73,7 @@ class Utils
             $istatus = isset($options[$currentValue]) && $currentValue != 0 && $currentValue != '' ? 'online' : 'offline';
             $element = '
                 <a class="status-toggle rex-' . $istatus . '" data-id="{{ID}}" data-status="' . $nvalue . '" data-table="{{TABLE}}">
-                    <i class="rex-icon rex-icon-' . $istatus . '"></i>&nbsp;<span class="text">' . $options[$currentValue] . '</span>
+                    <i class="rex-icon rex-icon-' . $istatus . '"></i>&nbsp;<span class="text">' . \rex_i18n::translate($options[$currentValue]) . '</span>
                 </a>
             ';
         }
